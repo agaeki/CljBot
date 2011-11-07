70b5b66c03e36933fc236c329a42d3ad8097b428
Nick Ridgway <agaeki.eegas@gmail.com>
2011-11-07 21:41:31 +0000

    Did some stuff
    Made it better

--- src/functions.clj
+++ src/functions.clj
@@ -17,6 +17,8 @@
 
 (defn command-nick [x] (let [y (seq (.split x " " 2))] (str "NICK " (nth y 1 "cljBot"))))
 
+(defn command-privmsg [x] (let [y (seq (.split x " " 3))] (str "PRIVMSG " (nth x 1 "") " :" (nth x 2 ""))))
+
 (defn process-PRIVMSG [x] (let [msg (dbg (seq (.split x " " 3)))] (if (= (str (nth (str (nth msg 1)) 1)) "?")
                                                         (try (eval (read-string (str "(#'cljBot/command-" (.toLowerCase (.substring (nth msg 1) 2)) " \"" (nth msg 0) " " (nth msg 2)"\")"))) (catch Exception e (.printStackTrace e)))
                                                         (str "-1"))))
